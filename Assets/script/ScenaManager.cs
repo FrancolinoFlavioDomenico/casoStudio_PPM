07315a48ecdb134f94b2b0487de6b1f46239113e
Flavio Francolino <francolinoflavio@gmail.com>
2022-09-05 17:05:31 +0200

    ripristinata grafica menu principale e inizializzazione implementazione del movimento dell'avatar

--- Assets/script/ScenaManager.cs
+++ Assets/script/ScenaManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class ScenaManager : MonoBehaviour
 {
     public void playGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2 );
+        SceneManager.LoadScene("scenaLivelli");
     }
 
     public void showCredits(){
