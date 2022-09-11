826bc38eb644a58cdda2b04ecbc2ebebb2ee5db4
Flavio Francolino <francolinoflavio@gmail.com>
2022-09-11 21:35:49 +0200

    conclusa implementazione del gameplay di base del livello 1

--- Assets/script/FollowPlayer.cs
+++ Assets/script/FollowPlayer.cs
@@ -8,12 +8,12 @@ public class FollowPlayer : MonoBehaviour
     Transform player;
     Vector3 velocity = Vector3.zero;
 
-    [SerializeField]
-    float zOffeset = -10f;
-    [SerializeField]
-    float xOffeset = 10f;
-    [SerializeField]
-    float yOffeset = 3;
+    Transform mainCamera;
+
+    [SerializeField]    
+    float zOffeset = -10f;  
+     [SerializeField]  
+    float yOffeset = 1f;
 
     [SerializeField]
     float smooth = -10f;//"velocita" con cui la camera deve seguire il player
@@ -22,11 +22,15 @@ public class FollowPlayer : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
     void Update()
     {
-        Vector3 targetPosition = new Vector3(player.position.x, player.position.y + yOffeset, player.position.z + zOffeset);//setto la posizione di default della main camera in modo tale da inquadrare il giocatore
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smooth);//quindi posizione la main camere sul player
+        //setto la posizione di default della main camera in modo tale da inquadrare il giocatore
+        Vector3 targetPosition = new Vector3(player.position.x , player.position.y + yOffeset, player.position.z + zOffeset);
+
+        //quindi posizione la main camere sul player
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smooth);
     }
 }
